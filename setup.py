1ecd50efc3ed26fa1f1d7792b2b1815136b1e1f7
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-08-20 23:24:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 setup(
     name="neural-diffeqs",
-    version="0.0.40",
+    version="0.1.0",
     python_requires=">3.6.0",
     author="Michael E. Vinyard - Harvard University - Broad Institute of MIT and Harvard - Massachussetts General Hospital",
     author_email="mvinyard@broadinstitute.org",
