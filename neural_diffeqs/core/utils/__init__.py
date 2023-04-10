1ef643915fe5ced18077f3413910451c6fdd1a0a
mvinyard <mvinyard.ai@gmail.com>
2023-04-10 21:16:44 +0000

    rm utils

--- neural_diffeqs/core/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-
-__module_name__ = "__init__.py"
-__doc__ = """Utility functions"""
-__author__ = ", ".join(["Michael E. Vinyard"])
-__email__ = ", ".join(["vinyard@g.harvard.edu"])
-
-
-# -- import modules: -----------------------------------------------------------
-from ._auto_parser import AutoParser
