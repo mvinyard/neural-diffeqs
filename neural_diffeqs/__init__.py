1df733b353ea52c9898b17cfce9f6be38a64341a
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-11-15 17:39:06 -0700

    Update __init__.py

--- neural_diffeqs/__init__.py
+++ neural_diffeqs/__init__.py
@@ -1,7 +1,6 @@
 
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["mvinyard.ai@gmail.com"])
-__version__ = "0.3.2"
 
 
 # -- import subpackages: -------------------------------------------------------
