934a8cd15582f8685b1205fd4d3fa3309024feb9
Michael Vinyard <mvinyard.ai@gmail.com>
2025-02-28 12:59:08 -0700

    rm bad tests; add version;

--- neural_diffeqs/__init__.py
+++ neural_diffeqs/__init__.py
@@ -2,6 +2,7 @@
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["mvinyard.ai@gmail.com"])
 
+from .__version__ import __version__
 
 # -- import subpackages: -------------------------------------------------------
 from . import core
@@ -14,3 +15,14 @@ from ._potential_sde import PotentialSDE
 from ._potential_ode import PotentialODE
 from ._latent_potential_ode import LatentPotentialODE
 from ._latent_potential_sde import LatentPotentialSDE
+
+__all__ = [
+    "NeuralODE",
+    "NeuralSDE",
+    "PotentialSDE",
+    "PotentialODE",
+    "LatentPotentialODE",
+    "LatentPotentialSDE",
+    "core",
+    "__version__",
+]
